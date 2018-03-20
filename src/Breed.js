af4195f2aa617752776a54b3c253883b5b9f635b
redaphid <iam@aaronherres.com>
2018-03-19 19:55:18 -1000

    mutations we might  wanna use

--- src/Breed.js
+++ src/Breed.js
@@ -2,16 +2,25 @@ import sample from 'lodash/fp/sample'
 import mean   from 'lodash/fp/mean'
 import map    from 'lodash/fp/map'
 import each   from 'lodash/fp/each'
+import random from 'lodash/fp/random'
 
+const randomWithFloat = random.convert({fixed: false})
 const eachWithKey = each.convert({cap: false})
 
 const Breed = (genomes) => {
   const child = {}
   const favoriteParent = sample(genomes)
+  const mutationRate = favoriteParent.mutationRate || 0
+
   eachWithKey((value, key) => {
+    if(randomWithFloat(0,1) < mutationRate) {
+      child[key] = randomWithFloat(0,1, true)
+      return
+    }
     const geneSelector = sample([sample, mean])
     const props = map(key, genomes)
     child[key] = geneSelector(props)
+
   }, favoriteParent)
 
   return child
--- src/Breed.test.js
+++ src/Breed.test.js
@@ -1,10 +1,11 @@
-import Breed  from './Breed'
-import times  from 'lodash/fp/times'
+import Breed from './Breed'
+import times from 'lodash/fp/times'
 import some from 'lodash/fp/some'
+import reject from 'lodash/fp/reject'
 
 describe('when breeding two creatures together', () => {
-  const genome1  = {luminosity: 0, hue: 0}
-  const genome2  = {luminosity: 1, hue: 1}
+  const genome1  = {mutationRate: 0.1, luminosity: 0, hue: 0}
+  const genome2  = {mutationRate: 0.1, luminosity: 1, hue: 1}
   const children =  times(()=> Breed([genome1, genome2]), 1000)
 
   it('should sometimes create a creature with a mixed luminosity', () => {
@@ -15,7 +16,7 @@ describe('when breeding two creatures together', () => {
       expect(some({luminosity: 0}, children)).toBe(true)
   })
 
-  it('should sometimes create a creature with a luminosity of 0', () => {
+  it('should sometimes create a creature with a luminosity of 1', () => {
       expect(some({luminosity: 1}, children)).toBe(true)
   })
 
@@ -27,7 +28,15 @@ describe('when breeding two creatures together', () => {
       expect(some({hue: 0}, children)).toBe(true)
   })
 
-  it('should sometimes create a creature with a hue of 0', () => {
+  it('should sometimes create a creature with a hue of 1', () => {
       expect(some({hue: 1}, children)).toBe(true)
+  })
+
+  it('should sometimes not be 0,1, or 0.5', () => {
+    const mutatedCreatures = reject({luminosity: 1},
+                              reject({luminosity: 0},
+                                reject({luminosity: 0.5}, children)))
+    expect(mutatedCreatures.length).not.toBe(0)
+
   })
 })
