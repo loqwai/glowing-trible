444ecc31f225626ef7338bb645516c05f9770bad
Roy van de Water <roy.v.water@gmail.com>
2018-03-19 18:54:17 -1000

    Remove things

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React, { Component } from "react"
 import times from "lodash/fp/times"
 import logo from "./logo.svg"
 import Creatures from "./Creatures"
-import "./App.css"
 
 class App extends Component {
   render() {
     const genomes = times(i => ({ luminosity: i / 10 }), 10)
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Creatures genomes={genomes} />
-      </div>
-    )
+    return <Creatures genomes={genomes} />
   }
 }
 
