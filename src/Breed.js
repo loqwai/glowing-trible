ae6fa44b78ba704ebe7249d88f4fe8a6821cf158
redaphid <iam@aaronherres.com>
2018-03-19 19:22:02 -1000

    variable properties

--- src/Breed.js
+++ src/Breed.js
@@ -2,12 +2,21 @@ import sample from 'lodash/fp/sample'
 import first  from 'lodash/fp/first'
 import last   from 'lodash/fp/last'
 import mean   from 'lodash/fp/mean'
-import map    from  'lodash/fp/map'
+import map    from 'lodash/fp/map'
+import each   from 'lodash/fp/each'
+
+const eachWithKey = each.convert({cap: false})
 
 const Breed = (genomes) => {
-  const luminosities = map('luminosity', genomes)
-  const geneSelector = sample([sample, mean])  
-  return {luminosity: geneSelector(luminosities)}
+  const child = {}
+  const favoriteParent = sample(genomes)
+  eachWithKey((value, key) => {
+    const geneSelector = sample([sample, mean])
+    const props = map(key, genomes)
+    child[key] = geneSelector(props)
+  }, favoriteParent)
+  
+  return child
 }
 
 export default Breed
--- src/Breed.test.js
+++ src/Breed.test.js
@@ -3,8 +3,8 @@ import times  from 'lodash/fp/times'
 import some from 'lodash/fp/some'
 
 describe('when breeding two creatures together', () => {
-  const genome1  = {luminosity: 0}
-  const genome2  = {luminosity: 1}
+  const genome1  = {luminosity: 0, hue: 0}
+  const genome2  = {luminosity: 1, hue: 1}
   const children =  times(()=> Breed([genome1, genome2]), 1000)
 
   it('should sometimes create a creature with a mixed luminosity', () => {
@@ -17,5 +17,17 @@ describe('when breeding two creatures together', () => {
 
   it('should sometimes create a creature with a luminosity of 0', () => {
       expect(some({luminosity: 1}, children)).toBe(true)
+  })
+
+  it('should sometimes create a creature with a mixed hue', () => {
+      expect(some({hue: 0.5}, children)).toBe(true)
+  })
+
+  it('should sometimes create a creature with a hue of 0', () => {
+      expect(some({hue: 0}, children)).toBe(true)
+  })
+
+  it('should sometimes create a creature with a hue of 0', () => {
+      expect(some({hue: 1}, children)).toBe(true)
   })
 })
