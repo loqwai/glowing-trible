21ad787f1b2fb31426012db255a61cdaae647b8a
Roy van de Water <roy.vwater@gmail.com>
2018-03-19 18:21:41 -1000

    Render a creature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import Creature from "./Creature"
+import "./App.css"
 
 class App extends Component {
   render() {
@@ -10,12 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Creature genome={{ luminosity: 0.8 }} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Creature.test.js
+++ src/Creature.test.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Creature from './Creature'
+import React from "react"
+import ReactDOM from "react-dom"
+import Creature from "./Creature"
 
-it('renders red without crashing', () => {
-  const div = document.createElement('div')
-  const gene = {color: 'red'}
-  ReactDOM.render(<Creature gene={gene} />, div)
+it("renders red without crashing", () => {
+  const div = document.createElement("div")
+  const gene = { color: "red" }
+  ReactDOM.render(<Creature genome={genome} />, div)
   ReactDOM.unmountComponentAtNode(div)
-});
+})
