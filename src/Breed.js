3c16b1e27c576f4ebec485ae4dda1783b8075e09
redaphid <iam@aaronherres.com>
2018-03-19 20:23:39 -1000

    maybe make mutation better

--- src/Breed.js
+++ src/Breed.js
@@ -3,9 +3,12 @@ import mean   from 'lodash/fp/mean'
 import map    from 'lodash/fp/map'
 import each   from 'lodash/fp/each'
 import random from 'lodash/fp/random'
-
+import clamp  from 'lodash/fp/clamp'
 const randomWithFloat = random.convert({fixed: false})
 const eachWithKey = each.convert({cap: false})
+
+const plus = (a,b) => a+b
+const minus = (a,b) => a+b
 
 const Breed = (genomes) => {
   const child = {}
@@ -13,16 +16,18 @@ const Breed = (genomes) => {
   const mutationRate = favoriteParent.mutationRate || 0
 
   eachWithKey((value, key) => {
-    if(randomWithFloat(0,1, true) < mutationRate) {      
-      child[key] = randomWithFloat(0,1, true)
-      return
-    }
     const geneSelector = sample([sample, mean])
     const props = map(key, genomes)
     child[key] = geneSelector(props)
 
-  }, favoriteParent)
+    if(randomWithFloat(0,1, true) < mutationRate) {
+      const mutationSelector = sample([plus, minus])
+      child[key] = mutationSelector(child[key], (child[key]/10) || 0.01)
+      child[key] = clamp(0,1,child[key])
+      return
+    }
 
+  }, favoriteParent)
   return child
 }
 
