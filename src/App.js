d1d105b782c7e50912a07ee8b84cb4c25b5c9112
Roy van de Water <roy.v.water@gmail.com>
2018-03-19 20:01:34 -1000

    Add mutationRate

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import React, { Component } from "react"
 import map from "lodash/fp/map"
 import times from "lodash/fp/times"
+import set from "lodash/fp/set"
+import styled from "styled-components"
 import logo from "./logo.svg"
 import Breed from "./Breed"
 import Creatures from "./Creatures"
+import GenerateCreature from "./GenerateCreature"
 
-const numCreatures = 84
+const numCreatures = 5
+const BreedingGround = styled.div`
+  display: flex;
+`
+
+const generateCreature = () => GenerateCreature(["red", "green", "blue"])
+
+const setMutation = set("mutationRate", 0.00000000000000001)
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      genomes: times(
-        i => ({
-          red: i / numCreatures,
-          green: i / numCreatures,
-          blue: i / numCreatures
-        }),
-        numCreatures
-      )
+      children: map(setMutation, times(generateCreature, numCreatures)),
+      parents: map(setMutation, times(generateCreature, numCreatures))
     }
   }
 
   render() {
-    const { genomes } = this.state
+    const { children, parents } = this.state
 
     const doBreed = () =>
       this.setState({
-        genomes: map(() => Breed(genomes), genomes)
+        children: map(() => Breed(children), children)
       })
 
     return (
       <div>
-        <Creatures genomes={genomes} />
+        <BreedingGround>
+          <Creatures genomes={children} />
+          <Creatures genomes={parents} />
+        </BreedingGround>
         <button onClick={doBreed}>Breed</button>
       </div>
     )
--- src/Creatures.js
+++ src/Creatures.js
@@ -1,6 +1,7 @@
 import React from "react"
-import Creature from "./Creature"
 import map from "lodash/fp/map"
+import styled from "styled-components"
+import Creature from "./Creature"
 
 const mapWithIndex = map.convert({ cap: false })
 
@@ -8,8 +9,14 @@ const renderCreatures = mapWithIndex((genome, i) => (
   <Creature genome={genome} key={i} />
 ))
 
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+`
+
 const Creatures = ({ genomes }) => {
-  return <div>{renderCreatures(genomes)}</div>
+  return <Column>{renderCreatures(genomes)}</Column>
 }
 
 export default Creatures
