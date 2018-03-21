0c6da7b4f25127b05ff3e36d70e54e51e0eb7e3a
Roy van de Water <roy.v.water@gmail.com>
2018-03-21 11:02:03 -1000

    Fix eyes clipping, with bonus cyclops feature

--- src/Creature/Eyes.js
+++ src/Creature/Eyes.js
@@ -3,24 +3,28 @@ import { formatHSL } from '../helpers/color'
 
 const eyeOffset = distance => distance * 30 + 10
 const formatCY = (position, vOffset) => vOffset + 10 + position * 40
-const formatLeftOffset = distance => 50 - eyeOffset(distance)
-const formatRightOffset = distance => 50 + eyeOffset(distance)
+const formatLeftOffset = (distance, r) => 50 + r - eyeOffset(distance)
+const formatRightOffset = (distance, r) => 50 - r + eyeOffset(distance)
 const formatR = size => size * 15 + 5
 
 const Eyes = ({ color, distanceBetweenEyes, position, size, vOffset }) => {
+  const r = formatR(size)
+  const cy = formatCY(position, vOffset)
+  const fill = formatHSL(color)
+
   return (
     <g>
       <circle
-        cx={formatLeftOffset(distanceBetweenEyes)}
-        cy={formatCY(position, vOffset)}
-        r={formatR(size)}
-        fill={formatHSL(color)}
+        cx={formatLeftOffset(distanceBetweenEyes, r)}
+        cy={cy}
+        r={r}
+        fill={fill}
       />
       <circle
-        cx={formatRightOffset(distanceBetweenEyes)}
-        cy={formatCY(position, vOffset)}
-        r={formatR(size)}
-        fill={formatHSL(color)}
+        cx={formatRightOffset(distanceBetweenEyes, r)}
+        cy={cy}
+        r={r}
+        fill={fill}
       />
     </g>
   )
