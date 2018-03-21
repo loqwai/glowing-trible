ce66dbade454a434dc8e2ed8c62ab371270bf318
Roy van de Water <roy.v.water@gmail.com>
2018-03-20 18:38:34 -1000

    Add mouth width

--- src/Creature/Mouth.js
+++ src/Creature/Mouth.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Mouth = ({ expression, position }) => {
+  const xOffset = position * 25
+  const x = parseInt(20 + xOffset)
+  const dx = parseInt(80 - xOffset)
   const y = parseInt(50 + position * 25)
   const curve = parseInt(50 + expression * 25)
 
   return (
     <path
-      d={`M 25 ${y} C 30 ${curve} 70 ${curve} 75 ${y}`}
+      d={`M ${x} ${y} C 30 ${curve} 70 ${curve} ${dx} ${y}`}
       fill="transparent"
       stroke="#000"
       strokeWidth="5"
