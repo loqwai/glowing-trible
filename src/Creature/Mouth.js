ef868a2c245b98106f6bd966c220436aec841e65
Roy van de Water <roy.v.water@gmail.com>
2018-03-20 18:41:59 -1000

    Add lip width

--- src/Creature/Mouth.js
+++ src/Creature/Mouth.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const Mouth = ({ expression, position }) => {
-  const xOffset = position * 25
-  const x = parseInt(20 + xOffset)
-  const dx = parseInt(80 - xOffset)
-  const y = parseInt(50 + position * 25)
-  const curve = parseInt(50 + expression * 25)
+const Mouth = ({ expression, position, width, lips }) => {
+  const xOffset = width * 25
+  const x = parseInt(20 + xOffset, 10)
+  const dx = parseInt(80 - xOffset, 10)
+  const y = parseInt(50 + position * 25, 10)
+  const curve = parseInt(50 + expression * 25, 10)
+  const stroke = parseInt(5 + lips * 10, 10)
 
   return (
     <path
       d={`M ${x} ${y} C 30 ${curve} 70 ${curve} ${dx} ${y}`}
       fill="transparent"
       stroke="#000"
-      strokeWidth="5"
+      strokeWidth={stroke}
       strokeLinecap="round"
     />
   )
