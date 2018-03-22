267465e941d58fa9474af31bb232b4594ec4fac9
redaphid <iam@aaronherres.com>
2018-03-21 14:22:48 -1000

    less logs

--- src/Fight.js
+++ src/Fight.js
@@ -1,4 +1,25 @@
-class Fight {
+import shuffle from 'lodash/fp/shuffle'
+import Round from './Round'
+
+const Fight = (creature1, creature2) => {
+  const log = []
+  let [attacker, defender] = shuffle([creature1, creature2])
+  let logEntry = {
+    attacker,
+    defender,
+    outcome: {attackerDamage: 0, defenderDamage: 0, action: 'start'}
+  }
+
+  log.push(logEntry)
+
+  while(logEntry.attacker.health > 0 && logEntry.defender.health > 0) {
+    logEntry = Round(attacker, defender)
+    attacker = logEntry.defender
+    defender = logEntry.attacker
+    log.push(logEntry)
+  }
   
+  return log
+
 }
 export default Fight
