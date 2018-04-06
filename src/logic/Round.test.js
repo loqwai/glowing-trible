b9c2ba89ceeca14e06b0596fd00d9cd9153041ec
Roy van de Water <roy.v.water@gmail.com>
2018-04-06 13:44:02 -0700

    Fix tests

--- src/logic/Round.test.js
+++ src/logic/Round.test.js
@@ -7,178 +7,118 @@ import GenerateCreature from './GenerateCreature'
 
 describe('When 2 creatures fight', () => {
   describe('When they are armless creatures', () => {
-    const armlessCreature = {
-      health: 100,
-      arms: 0,
-      body: 1,
-      legs: 0,
-    }
-    const {attacker, defender,  outcome} = Round(armlessCreature, armlessCreature)
-
-    it('should reduce the health of the attacking armless creature by 10 each turn', () => {
-        expect(attacker.health).toBeLessThan(100)
-    })
-
-    it('should emit the correct outcome', () => {
-        const armlessCreature = {
-          health: 100,
-          arms: 1,
-          body: 0,
-          legs: 0,
-        }
-        const {outcome} = Round(armlessCreature, armlessCreature)
-        expect(outcome.action).toBe('hits')
-        expect(outcome.defenderDamage).toBe(100)
-        expect(outcome.attackerDamage).toBeGreaterThan(0)
+    it('should reduce the health of the attacking creature by 10', () => {
+      const armlessCreature = {
+        health: 100,
+        arms: 0,
+        body: 1,
+        legs: 0,
+      }
+      const { outcome } = Round(armlessCreature, armlessCreature)
+      expect(outcome.attackerDamage).toBeCloseTo(14)
     })
   })
 
-  it('should reduce the health of the weaker attacking creature by 5 each turn', () => {
+  describe('when the creatures have no arms & legs and just a half body', () => {
+    it('should reduce the health of the weaker attacking creature by 5 each turn', () => {
       const armlessCreature = {
         health: 100,
         arms: 0,
         body: 0.5,
         legs: 0,
       }
-      const {attacker, defender} = Round(armlessCreature, armlessCreature)
-      expect(attacker.health).toBeLessThan(100)
+      const { outcome } = Round(armlessCreature, armlessCreature)
+      expect(outcome.attackerDamage).toBeCloseTo(7.5)
+    })
   })
 
-  it('should reduce the health of the ULTIMATE CREATURE by 30', () => {
-      const ultimateCreature = {
-        health: 100,
-        arms: 1,
-        body: 1,
-        legs: 1,
-      }
-      const {attacker, defender} = Round(ultimateCreature, ultimateCreature)
-      expect(attacker.health).toBeLessThan(100)
-  })
-
-  it('should let a strong creature hits a creature with a weak body for 10', () => {
-      const ultimateCreature = {
-        health: 100,
-        arms: 1,
-        body: 1,
-        legs: 1,
-      }
-
-      const pushupCreature = {
+  describe('When they have huge arms and no body', () => {
+    it('should do 65 damage', () => {
+      const creature = {
         health: 100,
         arms: 1,
         body: 0,
         legs: 0,
       }
-
-      const {attacker, defender} = Round(ultimateCreature, pushupCreature)
-      expect(defender.health).toBe(0)
+      const { outcome } = Round(creature, creature)
+      expect(outcome).toEqual({
+        action: 'hits',
+        defenderDamage: 65,
+        attackerDamage: 14,
+      })
+    })
   })
 
-  it('should let a kinda strong creature hits a creature with a weak body for 5', () => {
+  describe('when the creature has perfect everything', () => {
+    it('should reduce the health of the ULTIMATE CREATURE by 35.45', () => {
       const ultimateCreature = {
+        health: 100,
+        arms: 1,
+        body: 1,
+        legs: 1,
+      }
+      const { outcome } = Round(ultimateCreature, ultimateCreature)
+      expect(outcome.defenderDamage).toBeCloseTo(35.45)
+    })
+  })
+
+  describe('kinda strong ceature vs weak body', () => {
+    it('should do 32.5 damage', () => {
+      const creature = {
         health: 100,
         arms: 0.5,
-        body: 1,
-        legs: 1,
-      }
-
-      const pushupCreature = {
-        health: 100,
-        arms: 1,
         body: 0,
         legs: 0,
       }
 
-      const {attacker, defender} = Round(ultimateCreature, pushupCreature)
-      expect(defender.health).toBe(50)
+      const { outcome } = Round(creature, creature)
+      expect(outcome.defenderDamage).toBeCloseTo(32.5)
+    })
   })
 
-  describe('If the attacker dies before it can attack', () =>{
-    const ultimateCreature = {
-      health: 10,
-      arms: 0.5,
-      body: 1,
-      legs: 1,
-    }
-
-    const pushupCreature = {
-      health: 100,
-      arms: 1,
-      body: 0,
-      legs: 0,
-    }
-
-    const {attacker, defender, outcome} = Round(ultimateCreature, pushupCreature)
-    it('shouldnt hurt the defender', () => {
-        expect(defender.health).toBe(100)
-    })
-
+  describe('If the attacker dies before it can attack', () => {
     it('should emit the correct outcome', () => {
-        expect(outcome.action).toBe('starves')
-        expect(outcome.defenderDamage).toBe(0)
-        expect(outcome.attackerDamage).toBeGreaterThan(0)
+      const creature = {
+        health: 10,
+        arms: 1,
+        body: 1,
+        legs: 1,
+      }
+
+      const { outcome } = Round(creature, creature)
+      expect(outcome).toEqual({
+        action: 'starves',
+        attackerDamage: 40,
+        defenderDamage: 0,
+      })
     })
   })
 
-  it('If a hard-hitting creature hits a tank, it should absorb less damage', ()=> {
-    const pushupCreature = {
-      health: 100,
-      arms: 1,
-      body: 0,
-      legs: 0,
-    }
+  describe('a big arm creature hits a tank', () => {
+    it('it should absorb less damage', () => {
+      const creature = {
+        health: 100,
+        arms: 1,
+        body: 1,
+        legs: 0,
+      }
 
-    const tankCreature = {
-      health: 100,
-      arms: 0,
-      body: 1,
-      legs: 0,
-    }
-    const {attacker, defender} = Round(pushupCreature, tankCreature)
-    expect(defender.health).toBe(50)
-  })
-
-  it('If a weak creature hits a tank, it should not heal', ()=> {
-    const pushupCreature = {
-      health: 100,
-      arms: 0,
-      body: 0,
-      legs: 0,
-    }
-
-    const tankCreature = {
-      health: 100,
-      arms: 0,
-      body: 1,
-      legs: 0,
-    }
-    const {attacker, defender} = Round(pushupCreature, tankCreature)
-    expect(defender.health).toBe(100)
-  })
-
-  describe('when a hard-hitting creature is up against a runner', () => {
-    const pushupCreature = {
-      health: 100,
-      arms: 1,
-      body: 0,
-      legs: 0,
-    }
-    const runnerCreature = {
-      health: 100,
-      arms: 0,
-      body: 0,
-      legs: 1,
-    }
-    const results =  times(()=> Round(pushupCreature, runnerCreature), 1000)
-    it('the runner should never be hit', ()=> {
-      const averageDefenderHealth = mean(map('defender.health',results))
-      expect(averageDefenderHealth).toBe(0)
-    })
-
-    it('the runner should never run away', ()=> {
-      expect(some({action: 'hits'}, map('outcome', results))).toBeTruthy()
-      expect(some({action: 'misses'}, map('outcome', results))).toBeFalsy()
+      const { defender } = Round(creature, creature)
+      expect(defender.health).toBeCloseTo(64.545)
     })
   })
 
+  describe('when a weak creature hits a tank', () => {
+    it('should not do damage', () => {
+      const creature = {
+        health: 100,
+        arms: 0,
+        body: 1,
+        legs: 0,
+      }
+
+      const { outcome } = Round(creature, creature)
+      expect(outcome.defenderDamage).toBeCloseTo(0)
+    })
+  })
 })
