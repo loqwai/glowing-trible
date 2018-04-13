80cb1f9fa62d2ab9117d90c432270c93d768b478
redaphid <iam@aaronherres.com>
2018-04-12 17:49:07 -0700

    random glitching out

--- src/Drawing.js
+++ src/Drawing.js
@@ -19,6 +19,8 @@ class Scene extends Component {
     )
     BABYLON.SceneLoader.Load("models/", "fox.babylon", this.engine, (scene) => {
       this.scene = scene
+      const morphTargetManager = scene.meshes[0].morphTargetManager
+      window.morphTargetManager = morphTargetManager
       const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
       camera.setTarget(BABYLON.Vector3.Zero())
       camera.attachControl(this.canvas, true)
@@ -26,12 +28,15 @@ class Scene extends Component {
       var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
       // Default intensity is 1. Let's dim the light a small amount
-      light.intensity = 0.7;      
+      light.intensity = 0.7;
       // Resize the babylon engine when the window is resized
       window.addEventListener('resize', this.onResizeWindow);
 
       this.engine.runRenderLoop(() => {
            scene.render()
+           morphTargetManager.influences[0] = Math.random()
+           morphTargetManager.influences[1] = Math.random()
+           morphTargetManager.influences[2] = Math.random()
       })
     })
   }
