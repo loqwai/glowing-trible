50fc9de1d091e8629b059c993200dcaa39c37f11
redaphid <iam@aaronherres.com>
2018-04-12 17:40:30 -0700

    babylon

--- src/Drawing.js
+++ src/Drawing.js
@@ -1,86 +1,66 @@
 import React, { Component } from 'react'
 
-import * as three from 'three'
-import * as OBJLoader from 'three-obj-loader'
-OBJLoader(three)
+import * as BABYLON from 'babylonjs'
 
-class Drawing extends Component {
-  componentDidMount () {
-    this.three = three
-    // Set the scene size.
-    const WIDTH = 400;
-    const HEIGHT = 300;
+class Scene extends Component {
 
-    // Set some camera attributes.
-    const VIEW_ANGLE = 45;
-    const ASPECT = WIDTH / HEIGHT;
-    const NEAR = 0.1;
-    const FAR = 10000;
-
-    // Get the DOM element to attach to
-    const {renderContainer} = this.refs
-
-    // Create a WebGL renderer, camera
-    // and a scene
-    const renderer = new this.three.WebGLRenderer()
-    const camera =
-      new this.three.PerspectiveCamera(
-          VIEW_ANGLE,
-          ASPECT,
-          NEAR,
-          FAR
-      );
-
-    camera.position.z = 50
-    camera.position.y = 50
-    const scene = new this.three.Scene()
-
-    // Add the camera to the scene.
-    scene.add(camera);
-
-    // Start the renderer.
-    renderer.setSize(WIDTH, HEIGHT);
-
-    // Attach the renderer-supplied
-    // DOM element.
-    renderContainer.appendChild(renderer.domElement)
-    const ambientLight = new this.three.AmbientLight( 0xcccccc, 0.4 )
-		scene.add( ambientLight );
-
-    const animate = () => {
-      this.cube.rotateY(0.05)
-      console.log(camera.position)
-      renderer.render(scene, camera)
-      requestAnimationFrame(animate)
+  onResizeWindow = () => {
+    if (this.engine) {
+      this.engine.resize()
     }
-
-    const loader = new this.three.JSONLoader()
-    loader.load(process.env.PUBLIC_URL + '/models/fox.json', (geometry) => {
-      console.log(geometry)
-      const material = new this.three.MeshLambertMaterial( {
-				vertexColors: this.three.FaceColors,
-				morphTargets: true,
-				overdraw: 0.5
-			});
-			const cube = new this.three.Mesh( geometry, material );
-			cube.scale.set( 1.5, 1.5, 1.5 );
-      scene.add(cube)
-      window.cube = cube
-      this.cube = cube
-      camera.lookAt(cube.position)
-      animate()
-    })
-
   }
 
-  render() {
+  componentDidMount = () => {
+    this.engine = new BABYLON.Engine(
+        this.canvas,
+        true,
+        this.props.engineOptions,
+        this.props.adaptToDeviceRatio
+    )
+    BABYLON.SceneLoader.Load("models/", "fox.babylon", this.engine, (scene) => {
+      this.scene = scene
+      const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene)
+      camera.setTarget(BABYLON.Vector3.Zero())
+      camera.attachControl(this.canvas, true)
+      this.camera = camera
+      var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+      // Default intensity is 1. Let's dim the light a small amount
+      light.intensity = 0.7;      
+      // Resize the babylon engine when the window is resized
+      window.addEventListener('resize', this.onResizeWindow);
+
+      this.engine.runRenderLoop(() => {
+           scene.render()
+      })
+    })
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.onResizeWindow)
+  }
+
+  onCanvasLoaded =  (c) => {
+    if (c !== null) {
+      this.canvas = c
+    }
+  }
+
+  render () {
+    let { width, height} = this.props
+
+    let opts = {}
+
+    if (width !== undefined && height !== undefined) {
+      opts.width = width
+      opts.height = height
+    }
+
     return (
-      <div>
-        <h1>Drawing</h1>
-          <div ref="renderContainer"/>
-      </div>
+      <canvas
+        ref={this.onCanvasLoaded}
+      />
     )
   }
 }
-
-export default Drawing
+export default Scene
